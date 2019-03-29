f7470cb53f5e6dc30a7eb9cb65e59e79b932a9f2
Laura Amabili <lauraamabili@Lauras-MacBook-Air.local>
2019-03-29 11:52:42 +0100

    viva il gelato

---
+++ register.php
@@ -0,0 +1,18 @@
+<?php
+
+// PARAMETRI DA MODIFICARE
+$WEBHOOK_URL = 'https://{lauravibesbot}.herokuapp.com/execute.php';
+$BOT_TOKEN = '{892311523:AAEQYHdZPrL1z-5v5c33YEkvk68cNFW06CQ
+}';
+
+// NON APPORTARE MODIFICHE NEL CODICE SEGUENTE
+$API_URL = 'https://api.telegram.org/bot' . $BOT_TOKEN .'/';
+$method = 'setWebhook';
+$parameters = array('url' => $WEBHOOK_URL);
+$url = $API_URL . $method. '?' . http_build_query($parameters);
+$handle = curl_init($url);
+curl_setopt($handle, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($handle, CURLOPT_CONNECTTIMEOUT, 5);
+curl_setopt($handle, CURLOPT_TIMEOUT, 60);
+$result = curl_exec($handle);
+print_r($result);
