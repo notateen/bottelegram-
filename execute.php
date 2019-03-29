4e10b32302360c0febbb631e6062fd0d9857d4b1
Laura Amabili <lauraamabili@Lauras-MacBook-Air.local>
2019-03-29 21:06:59 +0100

    arrosticini

--- execute.php
+++ execute.php
@@ -1,46 +1,57 @@
 <?php
 $content = file_get_contents("php://input");
 $update = json_decode($content, true);
+
 if(!$update)
 {
-  exit;
+exit;
 }
+
 $message = isset($update['message']) ? $update['message'] : "";
 $messageId = isset($message['message_id']) ? $message['message_id'] : "";
 $chatId = isset($message['chat']['id']) ? $message['chat']['id'] : "";
+$senderId = isset($message['chat']['id']) ? $message['chat']['id'] : "";
 $firstname = isset($message['chat']['first_name']) ? $message['chat']['first_name'] : "";
 $lastname = isset($message['chat']['last_name']) ? $message['chat']['last_name'] : "";
 $username = isset($message['chat']['username']) ? $message['chat']['username'] : "";
 $date = isset($message['date']) ? $message['date'] : "";
 $text = isset($message['text']) ? $message['text'] : "";
-$text = trim($text);
-$text = strtolower($text);
-header("Content-Type: application/json");
-$response = '';
-if(strpos($text, "/start") === 0 || $text=="ciao")
-{
-	$response = "Ciao $firstname, benvenuto! \n
-		prova a scrivere: 		-sono triste \n
-						-stupiscimi \n
-						-fammi un complimento\n";
-}
-elseif($text=="sono triste")
-{
-	$response = "non esserlo, sei forte";
-}
-elseif($text=="fammi un complimento")
-{
-	$response = "sei bellissima";
-}
-elseif($text=="stupiscimi")
-{
-	$response = "sarà una giornata bellissima";
 
+if (!$text) {
+// Verificare che l'ultimo carattere sia un punto di domanda
+if ($text[sizeof($text) - 1] == '?') {
+// Da qui mandi la risposta
+$answer = $answers[rand(0, sizeof($answers)-1)];
+} else {
+// Da qui gli mandi "ehy fammi una domanda"
+$answer = "Per poterti rispondere, mi devi fare una domanda...";
 }
-else
-{
-	$response = "Comando non valido!";
 }
-$parameters = array('chat_id' => $chatId, "text" => $response);
+
+$answers = array(
+"Per quanto posso vedere, sì",
+"È certo",
+"È decisamente così",
+"Molto probabilmente",
+"Le prospettive sono buone",
+"I segni indicano di sì",
+"Senza alcun dubbio",
+"Sì",
+"Sì",
+"Ci puoi contare",
+"È difficile rispondere, prova di nuovo",
+"Rifai la domanda più tardi",
+"Meglio non risponderti adesso",
+"Non posso predirlo ora",
+"Concentrati e rifai la domanda",
+"Non ci contare",
+"La mia risposta è no",
+"Le mie fonti dicono di no",
+"Le prospettive non sono buone",
+"Molto incerto"
+);
+
+header("Content-Type: application/json");
+$parameters = array('chat_id' => $chatId, "text" => $answer);
 $parameters["method"] = "sendMessage";
 echo json_encode($parameters);
